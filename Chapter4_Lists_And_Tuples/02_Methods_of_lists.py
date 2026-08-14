3c809b036837ae62515adc529c2c8fd104672da2
Satyarth Upadhyay <Satyarthupadhyay31@gmail.com>
2026-08-14 16:20:57 +0530

    Methods of List

---
+++ Chapter4_Lists_And_Tuples/02_Methods_of_lists.py
@@ -0,0 +1,21 @@
+Fruits = ["Apple","Banana","Mango","Grapes"]
+print(Fruits)
+
+Fruits.append("Berry")     #This will add Berry in the Last
+print(Fruits)
+
+list = [2,3,4,6,9,0,5,]
+list.sort()                #Update the list to [0,2,3,4,5,6,9]
+print(list)
+
+list.reverse()             #Update the list to [9,6,5,4,3,2,0]
+print(list) 
+
+list.insert(3,7)           #Insert 7 on the 3rd index
+print(list)
+
+list.pop(2)                # Delete the element present at index 2
+print(list)
+
+list.remove(6)             # Delete the element 6 from the list
+print(list)
