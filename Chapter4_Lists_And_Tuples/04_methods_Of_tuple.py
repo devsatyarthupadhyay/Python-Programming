904aa4d03c75e86380c3142a8bb9259f1f681798
Satyarth Upadhyay <Satyarthupadhyay31@gmail.com>
2026-08-14 17:44:31 +0530

    methods of tuple

---
+++ Chapter4_Lists_And_Tuples/04_methods_Of_tuple.py
@@ -0,0 +1,10 @@
+a = (1,2,3,7.31,"Satyarth",3)
+print(a)
+
+count = a.count(3)      # Count how many times 3 comes in the tuple 
+print(count)
+count2 = a.count(1)
+print(count2)
+
+index = a.index(7.31)  # find index of 7.31 in tuple , if there are two 7.31 in tuple then it will show index of first 7.31 
+print(index)
