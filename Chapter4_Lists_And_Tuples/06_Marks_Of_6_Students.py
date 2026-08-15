ade5b418c7b55fc640558fa96657025ecd50d48b
Satyarth Upadhyay <Satyarthupadhyay31@gmail.com>
2026-08-15 09:21:15 +0530

    Input Marks Of 6 student in the Sorted manner

---
+++ Chapter4_Lists_And_Tuples/06_Marks_Of_6_Students.py
@@ -0,0 +1,22 @@
+Marks = []
+
+Student1  = int(input("Marks of the student1 :"))
+Marks.append(Student1)
+
+Student2  = int(input("Marks of the student2 :"))
+Marks.append(Student2)
+
+Student3  = int(input("Marks of the student3 :"))
+Marks.append(Student3)
+
+Student4  = int(input("Marks of the student4 :"))
+Marks.append(Student4)
+
+Student5  = int(input("Marks of the student5 :"))
+Marks.append(Student5)
+
+Student6  = int(input("Marks of the student6 :"))
+Marks.append(Student6)
+
+Marks.sort()
+print(Marks)
