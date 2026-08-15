25718fb925bc1086b682e3b591c57cf3f88b8892
Satyarth Upadhyay <Satyarthupadhyay31@gmail.com>
2026-08-15 09:05:07 +0530

    input by user in the list

---
+++ Chapter4_Lists_And_Tuples/05_Input_By_User.py
@@ -0,0 +1,18 @@
+Fruits = []
+
+f1 = input("Enter Fruit Name :")
+Fruits.append(f1)
+
+f2 = input("Enter Fruit Name :")
+Fruits.append(f2)
+
+f3 = input("Enter Fruit Name :")
+Fruits.append(f3)
+
+f4 = input("Enter Fruit Name :")
+Fruits.append(f4)
+
+f5 = input("Enter Fruit Name :")
+Fruits.append(f5)
+
+print(Fruits)
